8453aadfaf66e1d8661d0187ff0a824ae50d50c2
birds0501 <birds0501@naver.com>
2025-11-05 15:47:26 +0900

    최적화 완료-02

--- js/loading.js
+++ js/loading.js
@@ -1,29 +1,3 @@
-// $(function () {
-//   //로딩
-//   //대상을 변수에 저장
-//   const $window = $(window);
-//   const $body = $("body");
-//   const $loading = $(".loading"); //html에서 잘 돌아가는지 확인 후 스크립트로 붙이기(모든 페이지에 로딩 페이지가 들어가야 하므로)
-
-//   //body의 마지막 부분에 집어넣기
-//   $body.append($loading);
-//   const $target = $(".loading"); //값을 다시 담기
-
-//   //로딩이 완료되면 (Load)
-//   $window.on("load", function () {
-//     //너무 빨리 사라지므로 조금 이따가 사라지게끔(로딩 페이지 길게)
-//     //setTimeout(동작, 시간)
-//     setTimeout(function () {
-//       $target.fadeOut();
-
-//       //1초 후에 로딩요소 지우기
-//       setTimeout(function () {
-//         $target.remove();
-//       }, 500);
-//     }, 1000);
-//   });
-// });
-
 $(function () {
   const $window = $(window);
   const $body = $("body");
@@ -42,7 +16,7 @@ $(function () {
       $target.fadeOut(500, function () {
         $target.remove();
       });
-    }, 1000); // 1초 정도 여유 있게
+    }, 700); // 1초 정도 여유 있게
   });
 
   // 백업용 타이머 (혹시 load 이벤트가 안 오면 6초 뒤 강제 제거)
